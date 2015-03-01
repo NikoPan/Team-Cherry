c84574621ab7fb112d0a136f3f937c43a84d371c
unknown <kirill2ll@mail.ru>
2015-03-01 21:49:48 +0200

    adding jquery and hideShow to some menu

---
+++ version/JS/hideShow.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    var $allSections = $('section');
+    var $allList = $('aside nav ul li');
+    hideAll(0);
+    for (var i = 0; i < $allList.length; i++) {
+        // adding onclick function to all buttons
+        $allList[i].onclick = function (e) {
+            hideAll(1000);
+            var which = $(this).index();
+            $($allSections.get(which)).show(1000);
+        }
+    }
+    function hideAll(sec) {
+        for (var i = 0; i < $allSections.length; i++) {
+            $($allSections.get(i)).hide(sec);
+        }
+    }
+}());
